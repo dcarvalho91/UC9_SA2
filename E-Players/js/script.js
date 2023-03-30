e56e6bec2374fcc200cea4b6370d3eb307d7a0ec
David Carvalho <david.ef.carvalho@gmail.com>
2023-03-29 23:18:10 -0300

    Implementação Responsividade JS e Media Query

---
+++ E-Players/js/script.js
@@ -0,0 +1,19 @@
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value
+    alert(email)
+}
+
+function mostrarmenu($event){
+    let menu = document.getElementById("menu")
+   
+    /*if(menu.style.display == "none")*/
+
+    if(getComputedStyle(menu).display == "none") {
+        menu.style.display = "flex"
+    }
+    else{
+        menu.style.display = "none"
+    }
+
+    event.preventDefault();
+}
