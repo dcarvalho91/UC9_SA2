dc85c8daa7d84a93103cd28d2200feb12c01e95b
David Carvalho <david.ef.carvalho@gmail.com>
2023-04-14 00:11:00 -0300

    Implementação JQuery - Carrossel; Menu Hamb

--- E-Players/js/script.js
+++ E-Players/js/script.js
@@ -1,3 +1,47 @@
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-f1", "banner-cs", "banner-lol"]
+    
+    setInterval(mudarSlide, 2000);
+
+    function mudarSlide(){
+        //REMOVER O SLIDE ANTERIOR
+        if (slideAtual >0){
+            $("#banner-carrossel").removeClass(listaSlides[slideAtual -1])
+        }
+        else{
+            $("#banner-carrossel").removeClass(listaSlides[listaSlides.length -1])
+        }
+
+        //EXIBIR O SLIDE ATUAL
+        $("#banner-carrossel").addClass(listaSlides[slideAtual])
+        //INDICAR QUAL O SLIDE ATUAL
+        console.log("var:", slideAtual)
+        slideAtual ++
+            
+        if (slideAtual >2){
+            slideAtual = 0
+        }
+      
+    }
+
+
+
+    $("#barras").click(function(){
+        if ($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+        }
+        else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
+
+
+
+
+
 function cadastrarNewsletter() {
     let email = document.getElementById("campo-email").value
     alert(email)
